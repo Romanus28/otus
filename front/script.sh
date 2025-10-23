194127bfb253970cb6041bbe0ec36ffe5c90661e
Romanus28 <mannaz2012@mail.ru>
2025-10-23 16:31:21 +0300

    Update script.sh

--- front/script.sh
+++ front/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install nginx prometheus-node-exporter nfs-common -y
+apt update && apt install nginx prometheus-node-exporter nfs-common mysql-server -y
 
 cp ./otus/frontend/default /etc/nginx/sites-available/
 
@@ -15,3 +15,15 @@ dpkg -i ./nas/elk/filebeat_8.17.1_amd64.deb
 cp ./otus/frontend/filebeat.yml /etc/filebeat/
 
 systemctl restart filebeat
+
+systemctl start mysql
+
+cp ./otus/front/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+
+systemctl restart mysql
+
+mysql -u root << EOF
+
+CHANGE REPLICATION SOURCE TO SOURCE_HOST='192.168.11.23', SOURCE_USER='repl', SOURCE_PASSWORD='oTUSlave#2020', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
+START SLAVE;
+EOF
