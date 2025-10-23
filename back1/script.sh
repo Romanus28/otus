a1f26b3a52f4a052eb7167027341ff71945ae005
Romanus28 <mannaz2012@mail.ru>
2025-10-23 16:08:41 +0300

    Update script.sh

--- back1/script.sh
+++ back1/script.sh
@@ -2,6 +2,25 @@
 
 apt update
 
-apt install apache2 prometheus-node-exporter -y
+apt install apache2 prometheus-node-exporter mysql-server -y
 
-cp ./otus/backend1/index.html /var/www/html/
+cp ./otus/back/index1.html /var/www/html1/index.html
+cp ./otus/back/index2.html /var/www/html2/index.html
+cp ./otus/back/ports.conf /etc/apache2/
+cp ./otus/back/balance.conf /etc/apache2/sites-available/
+ln -s /etc/apache2/sites-available/balance.conf /etc/apache2/sites-enabled/balance.conf
+
+systemctl start mysql
+
+cp ./otus/back/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+
+systemctl restart mysql
+
+mysql << EOF
+
+CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'oTUSlave#2020';
+GRANT REPLICATION SLAVE ON *.* TO repl@'%';
+CREATE DATABASE otus;
+EOF
+
+mysql otus < ./otus/back/otus.sql
